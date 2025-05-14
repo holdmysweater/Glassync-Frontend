759998c84bbcc10006ca775525bcb35178140e29
Komarov Daniil <komarow.daniil@gmail.com>
2025-05-14 20:38:38 +0300

    feat: People class

---
+++ src/core/People.ts
@@ -0,0 +1,36 @@
+class People {
+  private static instance: People | null = null;
+  //private people: Person[] = [];
+  private profile: Profile;
+
+  private constructor(profile: Profile) {
+    this.profile = profile;
+  }
+
+  // public static getInstance(): People {
+  //   if (!People.instance) {
+  //     People.instance = new People();
+  //   }
+  //   return People.instance;
+  // }
+
+  public getPersonByUID(UID: number): Person | undefined {
+    // TODO
+    //return this.people.find(User => person.UID === UID);
+    return undefined;
+  }
+
+  public getPeopleByFilter(filter: PersonFilter): Person[] {
+    // TODO
+    return [];
+  }
+  //   return this.people.filter(person => {
+  //     return (
+  //       (!filter.name || person.name.includes(filter.name)) &&
+  //       (!filter.surname || person.surname.includes(filter.surname)) &&
+  //       (!filter.nickname || person.nickname.includes(filter.nickname)) &&
+  //       (!filter.email || person.email.includes(filter.email))
+  //     );
+  //   });
+  // }
+}
