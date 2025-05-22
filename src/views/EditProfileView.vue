13a1425403b845a3f56e4f233f7240d43560dacf
Komarov Daniil <komarow.daniil@gmail.com>
2025-05-23 01:29:30 +0300

    feat: EditProfileView

---
+++ src/views/EditProfileView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container mt-3">
+    <button
+      class="btn btn-outline-secondary mb-4 d-block mx-auto"
+      @click="goBack"
+      type="button"
+    >
+      &lt; Вернуться в профиль
+    </button>
+
+    <ProfileParametersForm :profile="profile" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import ProfileParametersForm from "@/components/ProfileParametersForm.vue";
+import type { Profile } from "@/core/Profile";
+import { defineProps } from "vue";
+
+defineProps<{
+  profile: Profile;
+}>();
+
+const router = useRouter();
+
+function goBack() {
+  // ToDo: логика перехода на страницу профиля
+  router.push("/profile");
+}
+</script>
