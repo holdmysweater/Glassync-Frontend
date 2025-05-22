8d85142949234353e8a897ce278ba1cc3df94ee8
Komarov Daniil <komarow.daniil@gmail.com>
2025-05-22 19:07:13 +0300

    feat: NotificationPlatformList component

---
+++ src/components/NotificationPlatformList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="card p-3"
+    style="width: 360px; max-height: 80vh; overflow-y: auto"
+  >
+    <div class="card-header text-center" style="background-color: #f8f9fa">
+      <h5 class="mb-0">Выбор платформы для уведомлений</h5>
+    </div>
+
+    <NotificationPlatformCard
+      v-for="platform in platforms"
+      :key="platform.getPlatformUID()"
+      :platform="platform"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { NotificationPlatform } from "@/core/NotificationPlatform";
+import NotificationPlatformCard from "./NotificationPlatformCard.vue";
+import { defineProps } from "vue";
+
+defineProps<{
+  platforms: NotificationPlatform[];
+}>();
+</script>
