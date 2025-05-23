4e76660f398f028e3f4f20027f08acef0783eaaf
Komarov Daniil <komarow.daniil@gmail.com>
2025-05-23 21:36:25 +0300

    feat: RegistrationView

---
+++ src/views/RegistrationView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container mt-5" style="max-width: 400px">
+    <RegistrationForm :profile="profile" />
+
+    <div class="text-center mt-2">
+      Уже есть аккаунт?
+      <a href="#" @click.prevent="goToLogin">Войти</a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import RegistrationForm from "@/components/RegistrationForm.vue";
+import type { Profile } from "@/core/Profile";
+import { defineProps } from "vue";
+
+const router = useRouter();
+
+defineProps<{ profile: Profile }>();
+
+function goToLogin() {
+  // ToDo: логика перехода на страницу авторизации
+  router.push("/login");
+}
+</script>
