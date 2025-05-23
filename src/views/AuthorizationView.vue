3502479a73d5346daa25b350ffdfb1320b73b9be
Komarov Daniil <komarow.daniil@gmail.com>
2025-05-23 23:15:12 +0300

    feat: AuthorizationView

---
+++ src/views/AuthorizationView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container mt-5" style="max-width: 400px">
+    <AuthorizationForm :profile="profile" />
+
+    <div class="text-center mt-2">
+      Вы не зарегистрированы?
+      <a href="#" @click.prevent="goToRegister">Регистрация</a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import AuthorizationForm from "@/components/AuthorizationForm.vue";
+import type { Profile } from "@/core/Profile";
+import { defineProps } from "vue";
+
+const router = useRouter();
+
+defineProps<{ profile: Profile }>();
+
+function goToRegister() {
+  // ToDo: логика перехода на страницу регистрации
+  router.push("/register");
+}
+</script>
